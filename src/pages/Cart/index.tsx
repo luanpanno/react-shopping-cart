4b3e00179f1781a9781cfc28f586890994a086fb
Luan Panno <luanpanno@gmail.com>
2021-07-17 02:18:05 -0300

    feat: cart page content

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import Loading from '@components/Loading';
+import NoContentText from '@components/NoContentText';
+import Content from '@containers/Content';
+
+import { useProduct } from '@contexts/ProductContext';
+
+import { Container } from './styles';
+
 const Cart = () => {
-  return <div>cart</div>;
+  const { search } = useLocation();
+  const { loadingProducts, listProducts, cartProducts } = useProduct();
+  const query = useMemo(() => new URLSearchParams(search).get('q'), [search]);
+
+  useEffect(() => {
+    listProducts();
+  }, [listProducts, query]);
+
+  return (
+    <Container>
+      <Content
+        title="Carrinho"
+        headerComplements={
+          <span>{cartProducts?.length} produtos adicionados</span>
+        }
+      >
+        {loadingProducts && cartProducts?.length <= 0 && <Loading />}
+
+        {!loadingProducts && cartProducts?.length <= 0 && (
+          <NoContentText>Carrinho vazio.</NoContentText>
+        )}
+      </Content>
+    </Container>
+  );
 };
 
 export default Cart;
